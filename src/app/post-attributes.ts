e9233bd307e2ec784d2d5a9f7f74fcd438295111
caohongsheng <caohongsheng@qiyi.com>
2024-11-14 09:50:48 +0800

    update

--- src/app/post-attributes.ts
+++ src/app/post-attributes.ts
@@ -2,8 +2,6 @@ export default interface PostAttributes {
   title: string;  // 标题
   slug: string;   // path
   date: string;   // 日期
-  description: string;  // 描述
-  coverImage: string;   // 封面
   tags: string[];       // 标签
   category: string;     // 分类
   pinned?: boolean;      // 是否置顶
