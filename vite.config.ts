17aa25ad030efa54c1e6ecaa6301eded9e4efce5
caohongsheng <caohongsheng@qiyi.com>
2024-11-15 13:48:34 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => ({
         },
       },
       prerender: {
-        routes: ['/blog', '/blog/2022-12-27-my-first-post'],
+        routes: ['/blog'],
       },
     }),
   ],
