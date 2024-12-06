822d63f279351f9a426434cc21fb3a34f5d0a0f0
caohongsheng <caohongsheng@qiyi.com>
2024-12-06 13:55:29 +0800

    update

--- src/app/pages/archives/index.page.ts
+++ src/app/pages/archives/index.page.ts
@@ -1,12 +1,12 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { DatePipe } from "@angular/common";
+import { CommonModule, DatePipe } from "@angular/common";
 import { BlogStore } from "../../store/blog.store";
 
 @Component({
   selector: 'app-archives',
   standalone: true,
-  imports: [RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe, CommonModule],
   templateUrl: './index.page.html',
   styleUrl: './index.page.css',
 })
@@ -15,7 +15,17 @@ export default class ArchivesComponent implements OnInit {
   readonly postCount = this.store.count();
   readonly archives = this.store.archives();
   readonly years = [...this.archives.keys()].sort().reverse();
+
+  readonly selectedYear = signal('');
   ngOnInit(): void {
+  }
+
+  toggleSelected(year: string) {
+    if (year == this.selectedYear()) {
+      this.selectedYear.set('');
+    } else {
+      this.selectedYear.set(year);
+    }
   }
 
 }
